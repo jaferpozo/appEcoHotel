0766da517afd184a82ba65328db35ad1ef63df43
Freddy Mac <freddycalderon1990@gmail.com>
2024-06-28 14:54:33 -0500

    1

--- lib/app/presentation/modules/login/local_widgets/wgLogin.dart
+++ lib/app/presentation/modules/login/local_widgets/wgLogin.dart
@@ -2,12 +2,11 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-
-
 import '../../../../core/app_config.dart';
 import '../../../../core/utils/responsiveUtil.dart';
 import '../../../../core/values/app_images.dart';
 import '../../../../core/values/app_strings.dart';
+import '../../../routes/app_routes.dart';
 import '../../../widgets/custom_widgets.dart';
 import '../../controllers.dart';
 
@@ -23,12 +22,12 @@ class WgLogin extends StatefulWidget {
 
   const WgLogin(
       {Key? key,
-      this.controllerUser,
-      this.controllerPass,
-      this.onPressed,
-      this.formKey,
-      this.ancho = 50.0,
-      this.mostrarFondo = false})
+        this.controllerUser,
+        this.controllerPass,
+        this.onPressed,
+        this.formKey,
+        this.ancho = 50.0,
+        this.mostrarFondo = false})
       : super(key: key);
 
   @override
@@ -43,6 +42,7 @@ class _WgLoginState extends State<WgLogin> {
 
     Widget desing = Column(
       children: <Widget>[
+        SizedBox(height: 220),
         ConstrainedBox(
           constraints: BoxConstraints(
             maxWidth: responsive.ancho! - widget.ancho,
@@ -65,7 +65,7 @@ class _WgLoginState extends State<WgLogin> {
                       fonSize: sizeTxt,
                       hitText: "Ingrese el usuario",
                       validar: (text) {
-                        if (text!.length >= 3 ) {
+                        if (text!.length >= 3) {
                           return null;
                         }
                         return "Usuario no válido";
@@ -99,19 +99,92 @@ class _WgLoginState extends State<WgLogin> {
                 ],
               )),
         ),
-        SizedBox(
-          height: responsive.altoP(4),
+        Align(
+          alignment: Alignment.centerRight,
+          child: TextButton(
+            onPressed: () {
+              // Implementar acción de "Forgot Password"
+            },
+            child: Text('Olvido su Contraseña..?',style: TextStyle(color: Colors.white),),
+          ),
         ),
         ConstrainedBox(
           constraints: BoxConstraints(
-            maxWidth: responsive.ancho! - 80,
-            minWidth: responsive.ancho! - 80,
+            maxWidth: responsive.ancho! - 10,
+            minWidth: responsive.ancho! - 60,
           ),
           child: BotonesWidget(
             iconData: Icons.arrow_forward,
-            padding: EdgeInsets.symmetric(horizontal: 40, vertical: 5),
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
             title: "INGRESAR",
             onPressed: widget.onPressed,
+          ),
+        ),
+        const SizedBox(height: 3),
+        const Row(
+          children: <Widget>[
+            Expanded(child: Divider()),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8.0),
+              child: Text('O'),
+            ),
+            Expanded(child: Divider()),
+          ],
+        ),
+        const SizedBox(height: 4),
+        ElevatedButton.icon(
+          onPressed: () {
+            // Implementar acción de "Continue with Google"
+          },
+          icon: Icon(Icons.g_mobiledata),
+          label: Text('Continuar con Google'),
+          style: ElevatedButton.styleFrom(
+            foregroundColor: Colors.black,
+            backgroundColor: Colors.white,
+            side: BorderSide(color: Colors.grey),
+            padding: EdgeInsets.symmetric(vertical: 16, horizontal: 50),
+          ),
+        ),
+        SizedBox(height: 4),
+        ElevatedButton.icon(
+          onPressed: () {
+            // Implementar acción de "Continue with Facebook"
+          },
+          icon: Icon(Icons.facebook),
+          label: Text('Continuar con Facebook'),
+          style: ElevatedButton.styleFrom(
+            foregroundColor: Colors.black,
+            backgroundColor: Colors.white,
+            side: BorderSide(color: Colors.grey),
+            padding: EdgeInsets.symmetric(vertical: 16, horizontal: 45),
+          ),
+        ),
+        SizedBox(height: 4),
+        ElevatedButton.icon(
+          onPressed: () {
+            // Implementar acción de "Continue with Apple"
+          },
+          icon: Icon(Icons.apple),
+          label: Text('Continuar con Apple'),
+          style: ElevatedButton.styleFrom(
+            foregroundColor: Colors.black,
+            backgroundColor: Colors.white,
+            side: BorderSide(color: Colors.grey),
+            padding: EdgeInsets.symmetric(vertical: 16, horizontal: 55),
+          ),
+        ),
+        SizedBox(height: 6),
+        Center(
+          child: TextButton(
+            onPressed: () {
+              Get.offAllNamed(AppRoutes.REGISTROUSUARIO);
+            },
+            child: Text(
+              'No tienes una Cuenta? Registrate..!!',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
           ),
         ),
       ],
@@ -119,18 +192,19 @@ class _WgLoginState extends State<WgLogin> {
 
     Widget wg = widget.mostrarFondo
         ? Stack(
-            children: [
-              Container(
-                height: responsive.alto! / 2,
-                width: responsive.ancho! - 100,
-                child: Image.asset(
-                  AppImages.imgarea,
-                  fit: BoxFit.cover,
-                ),
-              ),
-              desing
-            ],
-          )
+      children: [
+
+        Container(
+          height: responsive.alto! / 2,
+          width: responsive.ancho! - 100,
+          child: Image.asset(
+            AppImages.imgarea,
+            fit: BoxFit.cover,
+          ),
+        ),
+        desing,
+      ],
+    )
         : desing;
 
     return GetBuilder<LoginController>(
